d9ea45bf7ab96fe0acaddbb26db0879079c5608f
Jair Medina <54874269+JairMedina97@users.noreply.github.com>
2021-11-01 14:15:23 -0600

    Create method1.py
    Class + def(functions) = Methods, We created a method that multiplies Item (self) price * quantity

---
+++ method1.py
@@ -0,0 +1,20 @@
+#Creation of an object + define a method 
+# Returns x * y
+class Item:
+    def calculate_total_price(self, x, y): 
+        return x * y
+
+# Class + def(functions) = Methods
+# We created a method that multiplies Item (self) price * quantity
+
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
+print(item2.calculate_total_price(item2.price, item2.quantity))
